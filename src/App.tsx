ccfc34bff915ac60dc6fea2347aca1f4a1a07cfb
Ousmane Kanoute <MrGzima@outlook.fr>
2026-04-23 23:11:23 +0200

    Ajout du player à la selection d'une video

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import MainMenu from "./pages/MainMenu/MainMenu";
 import { useIsMobile } from "./hooks/useIsMobile";
+import ReactPlayer from "react-player";
 function App() {
   const isMobile = useIsMobile();
   return (
@@ -16,6 +17,10 @@ function App() {
           backgroundImage: "url(/images/TV-BORDER.webp)",
         }}
       />
+      {/* <ReactPlayer
+        style={{ height: "100%", width: "100%" }}
+        src="https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
+      /> */}
       <MainMenu />
     </div>
   );
--- src/pages/DVDPlayer/videos.ts
+++ src/pages/DVDPlayer/videos.ts
@@ -6,6 +6,7 @@ export interface Video {
   dateFilm: string;
   descriptionFilm: string;
   imageFilm: string | null;
+  urlFilm: string;
 }
 
 const videos: Video[] = [
@@ -16,6 +17,7 @@ const videos: Video[] = [
     descriptionFilm:
       "Film sélectionné parmis les 10 lauréats du concours 'Filme ton quartier'. ",
     imageFilm: imgLaBonneExpression,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "02",
@@ -23,6 +25,7 @@ const videos: Video[] = [
     dateFilm: "1/ 1/2000",
     descriptionFilm: "12:00AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "03",
@@ -30,6 +33,7 @@ const videos: Video[] = [
     dateFilm: "1/ 1/2000",
     descriptionFilm: "12:00AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "04",
@@ -37,6 +41,7 @@ const videos: Video[] = [
     dateFilm: "1/ 1/2000",
     descriptionFilm: "12:00AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "05",
@@ -44,6 +49,7 @@ const videos: Video[] = [
     dateFilm: "1/ 1/2000",
     descriptionFilm: "12:00AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "06",
@@ -51,6 +57,7 @@ const videos: Video[] = [
     dateFilm: "1/ 1/2000",
     descriptionFilm: "12:00AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "07",
@@ -58,6 +65,7 @@ const videos: Video[] = [
     dateFilm: "1/ 1/2000",
     descriptionFilm: "12:00AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "08",
@@ -65,6 +73,7 @@ const videos: Video[] = [
     dateFilm: "1/ 1/2000",
     descriptionFilm: "12:00AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "09",
@@ -72,6 +81,7 @@ const videos: Video[] = [
     dateFilm: "2/14/2000",
     descriptionFilm: "08:30AM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "10",
@@ -79,6 +89,7 @@ const videos: Video[] = [
     dateFilm: "3/ 5/2000",
     descriptionFilm: "09:00PM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "11",
@@ -86,6 +97,7 @@ const videos: Video[] = [
     dateFilm: "4/20/2000",
     descriptionFilm: "03:15PM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
   {
     numFilm: "12",
@@ -93,6 +105,7 @@ const videos: Video[] = [
     dateFilm: "5/ 1/2000",
     descriptionFilm: "07:45PM",
     imageFilm: null,
+    urlFilm: "https://vz-94180d5e-480.b-cdn.net/9e1f336b-ee8b-445a-ba92-12aa1c605a40/playlist.m3u8"
   },
 ];
 
